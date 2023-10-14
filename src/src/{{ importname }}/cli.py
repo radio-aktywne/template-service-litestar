297ca676cb99a7b11e672ef48bb73fd71667b6a0
Sebastian Pietras <me@spietras.dev>
2023-10-14 14:51:59 +0200

    Cleaned up everything (#7)

--- src/src/{{ importname }}/cli.py
+++ src/src/{{ importname }}/cli.py
@@ -1,9 +1,7 @@
 from typer import Typer
 
-from {{ importname }}.builder import Builder
 
-
-class CliBuilder(Builder[Typer]):
+class CliBuilder:
     """Builds the CLI app."""
 
     def build(self) -> Typer:
