9d77b5f487a74196aa010fee78aa9b413c3eff80
Sebastian Pietras <me@spietras.dev>
2023-12-18 21:37:53 +0000

    Fixed asyncio event loops in tests

--- src/tests/integration/test_ping.py
+++ src/tests/integration/test_ping.py
@@ -1,11 +1,12 @@
+import pytest
 from litestar.status_codes import HTTP_204_NO_CONTENT
 from litestar.testing import AsyncTestClient
 
 
+@pytest.mark.asyncio(scope="session")
 async def test_get(client: AsyncTestClient) -> None:
     """Test if GET /ping returns correct response."""
 
-    async with client as client:
-        response = await client.get("/ping")
-        assert response.status_code == HTTP_204_NO_CONTENT
-        assert len(response.content) == 0
+    response = await client.get("/ping")
+    assert response.status_code == HTTP_204_NO_CONTENT
+    assert len(response.content) == 0
